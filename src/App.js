884289c58e40cb4ea0ad53b4fca11846d9ec02e4
Bill Li <bill.l1564262@gmail.com>
2020-08-21 23:33:26 -0400

    Prepare for merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Navigation from './components/Navigation/Navigation'
+import Landing from './components/Landing/Landing';
+
+import * as ROUTES from './constants/routes'
 
 const App = () => {
-  return (
+  return (  
     <div className="App">
       <header className="App-header">
         owo
       </header>
+      <Router>
+      <Navigation />  
+      <hr />
+      <Route path={ROUTES.LANDING} component={Landing} />
+    </Router>
     </div>
   )
 }
