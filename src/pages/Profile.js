c95079bee42074eb9a71c1c7cd5321ec0f60a31b
Matthew Tam <mtam1222@gmail.com>
2020-08-22 16:13:28 -0400

    Added New Profile Page & Routing

---
+++ src/pages/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header'
+
+
+const Profile = () => { 
+
+    return (
+        <>
+            <Header />
+
+            <div>
+                Profile Info
+            </div>
+
+        </>
+    )
+}
+
+export default Profile
