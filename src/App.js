9f0cc8791b0aac40726e2b3f1be8390504082179
Aarish Adeel <aarish8@gmail.com>
2020-08-21 23:05:04 -0400

    Update app.js to anonymous function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-
+        <p></p>
       </header>
     </div>
   );
